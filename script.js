240c4ef782d85c679fcf5233712cfd696afc9226
AjiJeevan <hajeenajeevan@gmail.com>
2024-09-06 18:50:13 +0400

    Adding all the files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function checkOddOrEven(number){
+    if( number %2 == 0){
+        console.log("Even Number")
+    }
+    else{
+        console.log("Odd Number")
+    }
+}
+
+checkOddOrEven(146)
